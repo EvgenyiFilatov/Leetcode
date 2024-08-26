1d4bff9f452d931bfee14dbfcfb8f57225a86edf
Evgenyi Filatov <bleidin12345@yandex.ru>
2024-08-26 10:55:35 +0700

    26.08

--- roman_nums.py
+++ roman_nums.py
@@ -3,10 +3,8 @@ class Solution:
         rom_nums = {'I': 1, 'V': 5, 'X': 10,
                     'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         summa = 0
-        for i in range(len(list(s))):
-            if i == len(list(s)) - 1:
-                summa += rom_nums[s[i]]
-            elif rom_nums[s[i]] < rom_nums[s[i+1]]:
+        for i in range(len(s)):
+            if i < len(list(s)) - 1 and rom_nums[s[i]] < rom_nums[s[i+1]]:
                 summa -= rom_nums[s[i]]
             else:
                 summa += rom_nums[s[i]]
