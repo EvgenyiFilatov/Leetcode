33b70e9ccbd6413b79482d07d1e2392f43e39609
Evgenyi Filatov <bleidin12345@yandex.ru>
2024-08-26 10:57:03 +0700

    26.08

--- roman_nums.py
+++ roman_nums.py
@@ -2,9 +2,9 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         rom_nums = {'I': 1, 'V': 5, 'X': 10,
                     'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        summa = 0
+        summa: int = 0
         for i in range(len(s)):
-            if i < len(list(s)) - 1 and rom_nums[s[i]] < rom_nums[s[i+1]]:
+            if i < len(s) - 1 and rom_nums[s[i]] < rom_nums[s[i+1]]:
                 summa -= rom_nums[s[i]]
             else:
                 summa += rom_nums[s[i]]
