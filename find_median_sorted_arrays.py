260306152fc34669f705d1f815c551457d573d5c
Evgenyi Filatov <bleidin12345@yandex.ru>
2024-08-27 20:24:07 +0700

    27.08.2024

---
+++ find_median_sorted_arrays.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m, n = len(nums1), len(nums2)
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and nums2[j-1] > nums1[i]:
+                imin = i + 1
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                imax = i - 1
+            else:
+                if i == 0:
+                    left_max = nums2[j-1]
+                elif j == 0:
+                    left_max = nums1[i-1]
+                else:
+                    left_max = max(nums1[i-1], nums2[j-1])
+                if (m + n) % 2 == 0:
+                    if i == m:
+                        right_min = nums2[j]
+                    elif j == n:
+                        right_min = nums1[i]
+                    else:
+                        right_min = min(nums1[i], nums2[j])
+                    return (left_max + right_min) / 2.0
+                return left_max
+
+
+find = Solution()
+nums1 = [1, 3]
+nums2 = [2]
+print(find.findMedianSortedArrays(nums1, nums2))
