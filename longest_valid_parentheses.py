07a4994b5db87cd03ce062b47bbd02730415d3c4
Evgenyi Filatov <bleidin12345@yandex.ru>
2024-11-10 21:49:50 +0700

    edit

--- longest_valid_parentheses.py
+++ longest_valid_parentheses.py
@@ -1,4 +1,8 @@
+"""Хард задачка."""
+
+
 def run(s):
+    """Решение которое не проходит по времени."""
     def IsValid(item):
         delta = 0
         for ch in item:
@@ -39,3 +43,31 @@ def run(s):
     valid_items = FilterValidItemsOnly(items)
     longest_item = GetTheLongetsItem(valid_items)
     return len(longest_item)
+
+
+def run2(s):
+    """Решение подглядел, не проходит по памяти."""
+    N = len(s)
+    INF = 10**10
+
+    @cache
+    def run(idx, delta, taken):
+        if idx == N and delta != 0:
+            return -INF
+        if idx == N and delta == 0:
+            return 0
+        if delta < 0:
+            return -INF
+        if s[idx] == '(' and not taken:
+            return max(run(idx + 1, delta, 0), 1 + run(idx + 1, delta + 1, 1))
+        if s[idx] == ')' and not taken:
+            return max(run(idx + 1, delta, 0), 1 + run(idx + 1, delta - 1, 1))
+        if s[idx] == '(' and delta == 0:
+            return max(0, 1 + run(idx + 1, delta + 1, 1))
+        if s[idx] == ')' and delta == 0:
+            return max(0, 1 + run(idx + 1, delta - 1, 1))
+        if s[idx] == '(':
+            return 1 + run(idx + 1, delta + 1, 1)
+        return 1 + run(idx + 1, delta - 1, 1)
+
+    return run(0, 0, 0)
